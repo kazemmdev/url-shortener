859f7548a38d9a0f368978ce549ebf03204862de
Kazem Mirzaei <k90mirzaei@gmail.com>
2026-09-19 13:48:56 +0330

    fix: correct AppDbContext class definition and update Program.cs for DbContext registration

--- backend/DotnetApi/Data/AppDbContext.cs
+++ backend/DotnetApi/Data/AppDbContext.cs
@@ -1,8 +1,9 @@
 namespace DotnetApi.Data;
 
+using DotnetApi.Models;
+using Microsoft.EntityFrameworkCore;
 
-
-public class AppDbContext : DbContext
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
-    
+    public DbSet<Url> Urls => Set<Url>();
 }
--- backend/DotnetApi/Program.cs
+++ backend/DotnetApi/Program.cs
@@ -1,9 +1,11 @@
+using DotnetApi.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
-
-
 
 var connection = builder.Configuration.GetConnectionString("Default");
 
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(connection));
 
 var app = builder.Build();
 
